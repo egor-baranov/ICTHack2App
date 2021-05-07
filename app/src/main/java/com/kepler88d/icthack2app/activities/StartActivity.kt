073b8ea3f6aa71ea0a83ef5b3d38fedd63f49697
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-05-07 23:16:41 +0300

    🔨 Start Activity code prototype

--- app/src/main/java/com/kepler88d/icthack2app/activities/StartActivity.kt
+++ app/src/main/java/com/kepler88d/icthack2app/activities/StartActivity.kt
@@ -1,5 +1,69 @@
 package com.kepler88d.icthack2app.activities
 
 import android.app.Activity
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.transition.TransitionManager
+import android.view.View
+import com.google.android.material.transition.platform.MaterialArcMotion
+import com.google.android.material.transition.platform.MaterialContainerTransform
+import com.google.android.material.transition.platform.MaterialFade
+import com.google.android.material.transition.platform.MaterialFadeThrough
+import com.kepler88d.icthack2app.databinding.ActivityStartBinding
 
-class StartActivity : Activity() {}
+class StartActivity : Activity() {
+    lateinit var binding: ActivityStartBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityStartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.materialButton.setOnClickListener {
+            performPageTransition(binding.firstScreen, binding.secondScreen)
+        }
+
+        binding.backButton.setOnClickListener {
+            performPageTransition(binding.secondScreen, binding.firstScreen)
+        }
+
+        binding.materialButton2.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+        binding.specializationLayout.setOnClickListener{
+            performTransformAnimation(binding.card, binding.specializationList)
+        }
+
+        binding.specializationList.setOnClickListener {
+            performTransformAnimation(binding.specializationList, binding.card)
+        }
+    }
+
+    private fun performPageTransition(firstPage: View, secondPage: View) {
+        val fadeThrough = MaterialFadeThrough()
+
+        TransitionManager.beginDelayedTransition(binding.root, fadeThrough)
+
+        firstPage.visibility = View.GONE
+        secondPage.visibility = View.VISIBLE
+    }
+
+    private fun performTransformAnimation(firstView: View, secondView: View) {
+        val transform = MaterialContainerTransform().apply {
+            startView = firstView
+            endView = secondView
+
+            addTarget(secondView)
+
+            pathMotion = MaterialArcMotion()
+            scrimColor = Color.TRANSPARENT
+        }
+
+        TransitionManager.beginDelayedTransition(binding.root, transform)
+
+        firstView.visibility = View.GONE
+        secondView.visibility = View.VISIBLE
+    }
+}
