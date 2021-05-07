d4546b6fd6ea49407f13c27e1ad9cbae876aa055
VITYQ <vitaly.sorokin14@gmail.com>
2021-05-07 19:41:10 +0300

    🔨 Added Recycler Adapter

--- app/src/main/java/com/kepler88d/icthack2app/fragments/MainFragment.kt
+++ app/src/main/java/com/kepler88d/icthack2app/fragments/MainFragment.kt
@@ -5,11 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.kepler88d.icthack2app.R
+import com.kepler88d.icthack2app.adapters.RecyclerViewMainAdapter
+import com.kepler88d.icthack2app.databinding.FragmentMainBinding
+import com.kepler88d.icthack2app.databinding.ItemProjectBinding
 
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(R.layout.fragment_main) {
 
+    lateinit var binding: FragmentMainBinding
+    lateinit var adapter: RecyclerViewMainAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +26,16 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        adapter = RecyclerViewMainAdapter(context!!)
+        binding.recyclerviewMain.adapter = adapter
     }
 
 }
