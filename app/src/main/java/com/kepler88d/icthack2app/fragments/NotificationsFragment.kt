e400e39d3cde6a9c5603f41c71456ddc041baddc
VITYQ <vitaly.sorokin14@gmail.com>
2021-05-07 17:15:54 +0300

    Initial commit 🎉

---
+++ app/src/main/java/com/kepler88d/icthack2app/fragments/NotificationsFragment.kt
@@ -0,0 +1,27 @@
+package com.kepler88d.icthack2app.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kepler88d.icthack2app.R
+
+
+class NotificationsFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_notifications, container, false)
+    }
+
+
+}
