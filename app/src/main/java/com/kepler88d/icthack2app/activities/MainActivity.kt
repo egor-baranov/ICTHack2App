e51bb75ee8c3ceec1aa8a06964a4fc559e41ca36
VITYQ <vitaly.sorokin14@gmail.com>
2021-05-07 22:47:24 +0300

    🎉 Added requests

--- app/src/main/java/com/kepler88d/icthack2app/activities/MainActivity.kt
+++ app/src/main/java/com/kepler88d/icthack2app/activities/MainActivity.kt
@@ -2,6 +2,7 @@ package com.kepler88d.icthack2app.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
@@ -11,7 +12,18 @@ import com.kepler88d.icthack2app.R
 import com.kepler88d.icthack2app.databinding.ActivityMainBinding
 import com.kepler88d.icthack2app.fragments.MainFragment
 import com.kepler88d.icthack2app.fragments.NotificationsFragment
+import com.kepler88d.icthack2app.model.Project
+import com.kepler88d.icthack2app.model.Reply
+import com.kepler88d.icthack2app.model.RequestWorker
+import com.kepler88d.icthack2app.model.User
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import org.json.JSONObject
 
+
+val api = RequestWorker()
 class MainActivity : FragmentActivity() {
 
     lateinit var binding: ActivityMainBinding
@@ -23,6 +35,27 @@ class MainActivity : FragmentActivity() {
         binding.viewpagerMain.apply {
             adapter = pagerAdapter
         }
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val a = api
+            val str = a.getRepliesList()
+//            Log.d("sadasd", str)
+//            Log.d("sadasd", JSONObject(str).getString("name"))
+//            val user = User(
+//                id=JSONObject(str).getString("id"),
+//                password=JSONObject(str).getString("password"),
+//
+//                )
+
+
+//            Log.d("checkrequest", "JSON: ${b.description}")
+//            Log.d("checkrequest", "JSON: ${a.getRepliesList()}")
+//            Log.d("checkrequest", "JSON: ${a.getUsersList()}")
+//            Log.d("checkrequest", "JSON: ${a.getProjectById(1)}")
+//            Log.d("checkrequest", "JSON: ${a.getUserById(1)}")
+//            Log.d("checkrequest", "JSON: ${a.getReplyById(1)}")
+        }
+
     }
 
 
