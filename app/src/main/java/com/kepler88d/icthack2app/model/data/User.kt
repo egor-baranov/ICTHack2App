a3e210f2def7e3b090a59f865dffe909b362a574
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-05-08 14:50:19 +0300

    🔨 Serializable data classes: User, Project, Reply

---
+++ app/src/main/java/com/kepler88d/icthack2app/model/data/User.kt
@@ -0,0 +1,28 @@
+package com.kepler88d.icthack2app.model.data
+
+import com.google.gson.GsonBuilder
+import com.kepler88d.icthack2app.model.enumerations.UserSpecialization
+import kotlinx.serialization.Serializable
+import org.json.JSONObject
+
+@Serializable
+data class User(
+    val id: Int,
+    val firstName: String,
+    val lastName: String,
+    val password: String,
+    val profileDescription: String,
+    val rating: Float,
+    val specialization: UserSpecialization,
+    val githubProfileLink: String,
+) {
+    fun toJson(): JSONObject = JSONObject(toJsonString())
+    private fun toJsonString(): String = GsonBuilder().create().toJson(this)
+
+    companion object {
+        fun fromJson(jsonObject: JSONObject): User =
+            GsonBuilder().create().fromJson(jsonObject.toString(), User::class.java)
+
+        fun fromJsonString(jsonString: String): User = fromJson(JSONObject(jsonString))
+    }
+}
