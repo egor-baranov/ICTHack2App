435515236cfda1bd66c255bc37006bd8770c57ac
VITYQ <vitaly.sorokin14@gmail.com>
2021-05-08 18:37:35 +0300

    🔨 Added binding views

--- app/src/main/java/com/kepler88d/icthack2app/adapters/RecyclerViewMainAdapter.kt
+++ app/src/main/java/com/kepler88d/icthack2app/adapters/RecyclerViewMainAdapter.kt
@@ -1,19 +1,26 @@
 package com.kepler88d.icthack2app.adapters
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.kepler88d.icthack2app.databinding.FragmentMainBinding
 import com.kepler88d.icthack2app.databinding.ItemProjectBinding
+import com.kepler88d.icthack2app.model.data.Project
 
-class RecyclerViewMainAdapter (val context: Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class RecyclerViewMainAdapter (val context: Context, val list: List<Project>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
 
     inner class ProjectItem(val binding: ItemProjectBinding): RecyclerView.ViewHolder(binding.root){
         fun bind(position: Int){
-//            binding.textviewProjectName.text = "Project name $position"
+            binding.textViewTitle.text = "${list[position].name} id: ${list[position].id}"
+            binding.textViewDescription.text = list[position].description
+            binding.cardViewProject.setOnClickListener {
+
+            }
         }
     }
 
@@ -27,5 +34,5 @@ class RecyclerViewMainAdapter (val context: Context) : RecyclerView.Adapter<Recy
         (holder as ProjectItem).bind(position)
     }
 
-    override fun getItemCount() = 200
+    override fun getItemCount() = list.size
 }
