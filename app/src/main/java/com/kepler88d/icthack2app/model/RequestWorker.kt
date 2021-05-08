5b1faeccdb14f935c45532234a051a72554a287d
VITYQ <vitaly.sorokin14@gmail.com>
2021-05-08 17:57:15 +0300

    🔨 Fixed class

--- app/src/main/java/com/kepler88d/icthack2app/model/RequestWorker.kt
+++ app/src/main/java/com/kepler88d/icthack2app/model/RequestWorker.kt
@@ -1,16 +1,19 @@
 package com.kepler88d.icthack2app.model
 
-import com.kepler88d.icthack2app.model.data.Project
-import com.kepler88d.icthack2app.model.data.User
+import android.util.Log
 import io.ktor.client.*
 import io.ktor.client.features.*
+import io.ktor.client.features.get
+//import io.ktor.client.features.json.*
+//import io.ktor.client.features.json.serializer.*
 import io.ktor.client.request.*
 import io.ktor.http.*
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 
 class RequestWorker {
     private val client = HttpClient {
+//        install(JsonFeature){
+//            serializer = KotlinxSerializer()
+//        }
         defaultRequest {
             url {
                 protocol = URLProtocol.HTTPS
@@ -19,77 +22,33 @@ class RequestWorker {
         }
     }
 
-    fun getUserById(id: Int, handler: (User) -> Unit) {
-        GlobalScope.launch {
-            handler(client.get {
-                url("/users/getById")
-                parameter("id", id.toString())
-            })
-        }
-    }
+//    suspend fun getUsersList() = client.get<List<User>>{
+//        url("/users/list")
+//    }
+//
+//    suspend fun getUserById(id: Int) = client.get<String>{
+//        url("/users/getById")
+//        parameter("id", id.toString())
+//    }
+//
+//    suspend fun getProjectsList() = client.get<String>{
+//        url("/projects/list")
+//    }
+//
+//    suspend fun getProjectById(id: Int) = client.get<String>{
+//        url("/projects/getById")
+//        parameter("id", id.toString())
+//    }
+//
+//    suspend fun getRepliesList() = client.get<List<Reply>>{
+//        url("/reply/list")
+//    }
+//
+//    suspend fun getReplyById(id: Int) = client.get<Reply>{
+//        url("/reply/getById")
+//        parameter("id", id.toString())
+//    }
 
-    fun getProjectById(id: Int, handler: (Project) -> Unit) {
-        GlobalScope.launch {
-            handler(client.get {
-                url("/projects/getById")
-                parameter("id", id.toString())
-            })
-        }
-    }
 
-    fun getProjectList(handler: (List<Project>) -> Unit) {
-        GlobalScope.launch {
-            handler(client.get {
-                url("/projects/list")
-            })
-        }
-    }
 
-    fun addProject(name: String, description: String, githubProjectLink: String, ownerId: Int) {
-        GlobalScope.launch {
-            client.post<String> {
-                url("/projects/add")
-                parameter("name", name)
-                parameter("description", description)
-                parameter("githubProjectLink", githubProjectLink)
-                parameter("ownerId", ownerId.toString())
-            }
-        }
-    }
-
-    fun registerUser(
-        id: Int,
-        firstName: String,
-        lastName: String,
-        password: String,
-        profileDescription: String,
-        specialization: String,
-        rating: Float,
-        githubProfileLink: String,
-        handler: (String) -> Unit
-    ) {
-        GlobalScope.launch {
-            handler(client.post {
-                url("/users/register")
-                parameter("id", id.toString())
-                parameter("firstName", firstName)
-                parameter("lastName", lastName)
-                parameter("password", password)
-                parameter("profileDescription", profileDescription)
-                parameter("specialization", specialization)
-                parameter("rating", rating.toString())
-                parameter("githubProfileLink", githubProfileLink)
-            })
-        }
-    }
-
-    fun authorizeUser(id: Int, password: String, handler: (User) -> Unit) {
-        GlobalScope.launch {
-            handler(User.fromJsonString(client.post {
-                url("/users/login")
-                parameter("id", id.toString())
-                parameter("password", password)
-            }))
-        }
-    }
 }
