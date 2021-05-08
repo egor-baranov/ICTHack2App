f8abac62b502532723fe06caae037270d46c8080
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-05-08 18:25:34 +0300

    🔨 Getting of a project list inside of MainFragment

--- app/src/main/java/com/kepler88d/icthack2app/fragments/MainFragment.kt
+++ app/src/main/java/com/kepler88d/icthack2app/fragments/MainFragment.kt
@@ -1,6 +1,7 @@
 package com.kepler88d.icthack2app.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +10,8 @@ import com.kepler88d.icthack2app.R
 import com.kepler88d.icthack2app.activities.MainActivity
 import com.kepler88d.icthack2app.adapters.RecyclerViewMainAdapter
 import com.kepler88d.icthack2app.databinding.FragmentMainBinding
+import com.kepler88d.icthack2app.model.RequestWorker
+import com.kepler88d.icthack2app.model.data.Project
 
 class MainFragment : Fragment(R.layout.fragment_main) {
     lateinit var binding: FragmentMainBinding
@@ -19,32 +22,23 @@ class MainFragment : Fragment(R.layout.fragment_main) {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentMainBinding.inflate(layoutInflater)
-
-//        binding.searchBox.setOnClickListener {
-//            (requireActivity() as MainActivity).performTransformAnimation(
-//                binding.searchBox,
-//                (requireActivity() as MainActivity).binding.searchCardView
-//            )
-//        }
-
-//        (requireActivity() as MainActivity).binding.searchCardView.setOnClickListener {
-//            (requireActivity() as MainActivity).performTransformAnimation(
-//                (requireActivity() as MainActivity).binding.searchCardView,
-//                binding.searchBox
-//            )
-//        }
-
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         adapter = RecyclerViewMainAdapter(context!!)
+
+        RequestWorker.getProjectList({ projectList: List<Project> ->
+            run {
+                Log.d("ProjectList", projectList.toString())
+            }
+        })
+
         binding.recyclerviewMain.adapter = adapter
         val pager = (activity as MainActivity).binding.viewpagerMain
         binding.buttonNotifications.setOnClickListener {
             pager.setCurrentItem(2, true)
         }
-
     }
 }
