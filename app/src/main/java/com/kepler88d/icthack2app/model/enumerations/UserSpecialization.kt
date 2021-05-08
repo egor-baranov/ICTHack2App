02f29a8e17bb42a47bcd26ccb341d3232c88882d
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-05-08 15:00:34 +0300

    🔨 ProjectTags and UserSpecialization enum classes

---
+++ app/src/main/java/com/kepler88d/icthack2app/model/enumerations/UserSpecialization.kt
@@ -0,0 +1,9 @@
+package com.kepler88d.icthack2app.model.enumerations
+
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+enum class UserSpecialization {
+    IOS_DEVELOPER, ANDROID_DEVELOPER, WEB_BACKEND_DEVELOPER, WEB_FRONTEND_DEVELOPER
+}
