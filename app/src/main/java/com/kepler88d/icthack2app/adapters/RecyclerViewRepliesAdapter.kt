b21f082669aed1137ac6052aaa4e1a4ac0891f45
VITYQ <vitaly.sorokin14@gmail.com>
2021-05-09 04:44:23 +0300

    🔨 Prototype notifications

--- app/src/main/java/com/kepler88d/icthack2app/adapters/RecyclerViewRepliesAdapter.kt
+++ app/src/main/java/com/kepler88d/icthack2app/adapters/RecyclerViewRepliesAdapter.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.kepler88d.icthack2app.R
+import com.kepler88d.icthack2app.activities.MainActivity
 import com.kepler88d.icthack2app.databinding.ItemReplyBinding
 
 class RecyclerViewRepliesAdapter(val context: Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
@@ -13,6 +16,28 @@ class RecyclerViewRepliesAdapter(val context: Context) : RecyclerView.Adapter<Re
         fun bind(position: Int){
             binding.buttonDelete.setOnClickListener {
                 notifyItemRemoved(position)
+            }
+            binding.itemReply.setOnClickListener {
+                val bottomSheetBehavior = BottomSheetBehavior.from((context as MainActivity).binding.bottom.bottomSheet)
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+            }
+            when(position%4){
+                1 -> {
+                    binding.textviewProject.text = "ЗАПРОС"
+                    binding.textviewProject.setTextColor(context.getColor(R.color.state_request))
+                }
+                2 -> {
+                    binding.textviewProject.text = "В ОЖИДАНИИ"
+                    binding.textviewProject.setTextColor(context.getColor(R.color.state_waiting))
+                }
+                3 -> {
+                    binding.textviewProject.text = "ПРИНЯТО"
+                    binding.textviewProject.setTextColor(context.getColor(R.color.state_accepted))
+                }
+                0 -> {
+                    binding.textviewProject.text = "ОТКЛОНЕНО"
+                    binding.textviewProject.setTextColor(context.getColor(R.color.state_rejected))
+                }
             }
         }
     }
